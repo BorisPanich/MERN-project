f4eee9879aa5e61a4b51eea565d0b9fceebaa5b7
BorisPanich <panich2303@gmail.com>
2021-08-13 05:40:22 +0300

    commit start

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+// constants
+const PORT = config.get('port') || 5000
+
+async function start() {
+    try {
+    //    address connect to Uri
+    await mongoose.connect(config.get('mongoUri'), {
+
+    })
+    } catch (e) {
+        console.log('Server Error', e.message)
+        // exit Node.js
+        process.exit(1)
+    }
+}
+
+start();
+
+// customization server
+app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
