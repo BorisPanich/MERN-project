4dcbae0264103444cf93d70f4daa63f639ae21d5
BorisPanich <panich2303@gmail.com>
2021-08-14 08:32:46 +0300

    component AuthPage

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 import "materialize-css";
 
 export const AuthPage = () => {
+    const [form, setForm] = useState({
+        email: "",
+        password: ""
+    });
+
+    const changeHandler = (e) => {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+
     return (
-        <div>
-            <h1>Auth Page</h1>
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h1>Auth Page</h1>
+                <div className="card blue lighten-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">Authorization</span>
+                        <div>
+                            <div className="input-field">
+                                <input
+                                    placeholder="Input email"
+                                    id="email"
+                                    type="text"
+                                    name="email"
+                                    className="yellow-input"
+                                    onChange={changeHandler}
+                                />
+                                <label htmlFor="email">Email</label>
+                                <div className="input-field">
+                                    <input
+                                        placeholder="Input password"
+                                        id="password"
+                                        type="password"
+                                        name="password"
+                                        className="yellow-input"
+                                        onChange={changeHandler}
+                                    />
+                                    <label htmlFor="password">Password</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-action">
+                        <button className="btn yellow darken-3" style={{marginRight: 10}}>Login</button>
+                        <button className="btn grey lighten-4 black-text">Registration</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- client/src/routes.js
+++ client/src/routes.js
@@ -5,7 +5,7 @@ import {LinksPage} from "./pages/LinksPage";
 import {DetailPage} from "./pages/DetailPage";
 import {AuthPage} from "./pages/AuthPage";
 
-export const useRoutes = isAuthenticated => {
+export const useRoutes = (isAuthenticated) => {
     if (isAuthenticated) {
         return (
             <Switch>
