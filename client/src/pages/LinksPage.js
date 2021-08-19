bad0674f9600bae4c65ebb28bc89422f3d38b1cd
BorisPanich <panich2303@gmail.com>
2021-08-19 06:54:12 +0300

    component LinksList

--- client/src/pages/LinksPage.js
+++ client/src/pages/LinksPage.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, {useCallback, useContext, useEffect, useState} from "react";
 import "materialize-css";
+import {useHttp} from "../hooks/http.hook";
+import {AuthContext} from "../context/AuthContext";
+import {Loader} from "../component/Loader";
+import {LinkCard} from "../component/LinkCard";
+import {LinksList} from "../component/LinksList";
 
 export const LinksPage = () => {
+    const [links, setLinks] = useState([]);
+    const {loading, request} = useHttp();
+    const {token} = useContext(AuthContext);
+
+    // downloader list links
+    const fetchLinks = useCallback(async () => {
+        try {
+            const fetched = await request(`/api/link/`, 'GET', null, {
+                // authorization
+                Authorization: `Bearer ${token}`
+            })
+            setLinks(fetched)
+        } catch (e) {
+        }
+    }, [token, request])
+
+    useEffect(() => {
+        fetchLinks()
+    }, [fetchLinks])
+
+    if (loading) {
+        return <Loader />
+    }
+
     return (
-        <div>
-            <h1>Links Page</h1>
-        </div>
+        <>
+            { !loading && <LinksList links={links} /> }
+        </>
     );
 }
 
