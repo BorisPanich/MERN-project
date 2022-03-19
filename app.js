e0e50b23be64ec14201e78bb8712b9aa60f04b46
BorisPanich <panich2303@gmail.com>
2022-03-19 07:13:33 +0300

    MinCellOfArray

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const config = require('config')
+const path = require('path')
 const mongoose = require('mongoose')
 
 const app = express()
 
 // parsing req.body (для исключения ошибки в режиме undefined)
-app.use(express.json({extended: true}))
+app.use(express.json({ extended: true }))
 
 // register ROUTE (connecting middleware)
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/redirect.routes'))
+
+//for work forntend and backend simultaneously
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 // constants
 const PORT = config.get('port') || 5000
